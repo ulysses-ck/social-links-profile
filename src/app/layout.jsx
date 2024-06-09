0b67af949f6550fae2ef3390bbffaceccf7d9d56
ulysses-ck <ulisesapaza.280@gmail.com>
2024-06-09 14:10:58 -0300

    refactor: Update body class in RootLayout component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-[#181818] p-20 flex justify-center items-center`}>{children}</body>
     </html>
   );
 }
